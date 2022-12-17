79ce147773e5a79d3ad1fdfda9103c75ced26340
Marta Stankevich <you@example.com>
2022-12-17 16:54:57 +0100

    implementacja wstecznego podstawienia

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -10,11 +10,19 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				 */
 
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	 int n = mat -> r;
+    int m = b -> r;
+    if (n != m || mat->r != mat->c || b->c != 1) // jezeli mat nie jest macierza kwadratowa, lub liczba wierszy mat != liczbie werszy b, lub b ma wiecej niz 1 kolumne
+        return 2;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (mat->data[i][i] == 0)
+            return 1;
+        double ans = b->data[i][0];
+        for (int j = 0; j < n; j++)
+            if (i != j) ans -= mat->data[i][j] * x->data[j][0];
+        x->data[i][0] = ans / mat->data[i][i];
+    }
 
 				return 0;
 }
