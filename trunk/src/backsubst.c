e3c64859b8cb253109cf5f11e24041af445e4d24
Marta Stankevich <you@example.com>
2022-12-20 00:59:42 +0100

    remove comments

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -5,14 +5,10 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+				
 	 int n = mat -> r;
     int m = b -> r;
-    if (n != m || mat->r != mat->c || b->c != 1) // jezeli mat nie jest macierza kwadratowa, lub liczba wierszy mat != liczbie werszy b, lub b ma wiecej niz 1 kolumne
+    if (n != m || mat->r != mat->c || b->c != 1) 
         return 2;
     for (int i = n - 1; i >= 0; i--)
     {
@@ -23,6 +19,7 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
             if (i != j) ans -= mat->data[i][j] * x->data[j][0];
         x->data[i][0] = ans / mat->data[i][i];
     }
+
 
 				return 0;
 }
