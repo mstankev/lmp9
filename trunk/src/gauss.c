ed4388599379c431c12b5fbcbb51efad594c1a99
KirilHorobets <120606486+KirilHorobets@users.noreply.github.com>
2022-12-21 17:17:10 +0100

    Update gauss.c

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,14 +1,31 @@
+#include <math.h>
+#include <stdio.h>
 #include "gauss.h"
-
+#include <math.h>
+#include "mat_io.h"
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate( Matrix *mat, Matrix *b){
+    int k, j, n, i;
+    double d;
+    n=mat->r;
 
-		return 0;
+    for(k = 0; k < n; k++){
+       
+        for(j = k + 1; j < n; j++){
+            if(mat->data[k][k] == 0){
+                return 1;
+            }
+
+            d = mat->data[j][k] / mat->data[k][k];
+            for(i = k; i < n; i++){
+                mat->data[j][i] = mat->data[j][i] -d * (mat->data[k][i]);
+            }
+            b->data[j][0] = b->data[j][0] - d * (b->data[k][0]);
+        }
+    }
+
+    return 0;
 }
-
